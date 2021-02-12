01c4ec18ef5e7e8cc86b6edaf9084068c41fcb70
polvora-fosforo <contaparacodigo999@gmail.com>
2021-02-12 09:13:12 -0300

    testing API

---
+++ js/controllers/controller.js
@@ -0,0 +1,89 @@
+import Model from '../models/model';
+
+import sunImg from '../../images/illustrations/sun.png';
+import waterImg from '../../images/illustrations/wateringcan.png';
+import dogImg from '../../images/illustrations/dog.png';
+
+const modelInstace = new Model();
+
+class controller {
+  constructor(products) {
+    this.products = products;
+  }
+
+  filters = [
+    {
+      name: "sun",
+      label: "Set the amount of sunlight your plant will get",
+      image: sunImg,
+      options: [
+        {
+          value: "no",
+          text: "No sunlight",
+        },
+        {
+          value: "low",
+          text: "Low sunlight",
+        },
+        {
+          value: "high",
+          text: "High sunlight",
+        },
+      ]
+    },
+    {
+      name: "water",
+      label: "How often do you want to water your plant?",
+      image: waterImg,
+      options: [
+        {
+          value: "rarely",
+          text: "Rarely",
+        },
+        {
+          value: "regularly",
+          text: "Regularly",
+        },
+        {
+          value: "daily",
+          text: "Daily",
+        },
+      ]
+    },
+    {
+      name: "pets",
+      label: "Do you have pets? Do they chew plants?",
+      image: dogImg,
+      options: [
+        {
+          value: true,
+          text: "Yes",
+        },
+        {
+          value: false,
+          text: "No/They don't care",
+        },
+      ]
+    },
+  ];
+  
+  changeValue(name, value) {
+    this[name] = value;
+
+    if((this.sun && this.sun !== '0') && (this.water && this.water !== '0') && (this.pets && this.pets !== '0')) {
+      this.products = modelInstace.fetchResults(
+        this.sun,
+        this.water,
+        this.pets
+      ).then(data => {
+        this.products = data;
+
+        // notify
+        console.log(this.products);
+      });
+    }
+
+  }
+}
+
+export default controller;
--- js/views/filtersView.js
+++ js/views/filtersView.js
@@ -1,102 +1,57 @@
-import sunImg from './images/illustrations/sun.png';
-import waterImg from './images/illustrations/wateringcan.png';
-import dogImg from './images/illustrations/dog.png';
+import controller from '../controllers/controller';
 
-const filtersForm = document.querySelector("#filtersForm");
+const controllerInstance = new controller();
 
-const filters = [
-  {
-    name: "sun",
-    label: "Set the amount of sunlight your plant will get",
-    image: sunImg,
-    options: [
-      {
-        value: "no",
-        text: "No sunlight",
-      },
-      {
-        value: "low",
-        text: "Low sunlight",
-      },
-      {
-        value: "high",
-        text: "High sunlight",
-      },
-    ]
-  },
-  {
-    name: "water",
-    label: "How often do you want to water your plant?",
-    image: waterImg,
-    options: [
-      {
-        value: "rarely",
-        text: "Rarely",
-      },
-      {
-        value: "regularly",
-        text: "Regularly",
-      },
-      {
-        value: "daily",
-        text: "Daily",
-      },
-    ]
-  },
-  {
-    name: "pets",
-    label: "Do you have pets? Do they chew plants?",
-    image: dogImg,
-    options: [
-      {
-        value: true,
-        text: "Yes",
-      },
-      {
-        value: false,
-        text: "No/They don't care",
-      },
-    ]
-  },
-];
+class filtersView {
+  renderFilters() {
 
-const changeValue = (value) => {
-  console.log(value);
+    const filtersForm = document.querySelector("#filtersForm");
+    
+    let filtersOptions = [];
+    
+    controllerInstance.filters.map((item, index) => {
+    
+      const divEl = document.createElement("div");
+    
+      const imgEl = document.createElement("img");
+      imgEl.setAttribute("src", item.image);
+      imgEl.setAttribute("alt", item.name);
+    
+      divEl.appendChild(imgEl);
+    
+      const labelEl = document.createElement("label");
+      labelEl.innerText = `${index + 1}. ${item.label}`;
+    
+      divEl.appendChild(labelEl);
+    
+      const selectEl = document.createElement("select");
+      selectEl.addEventListener("change", (e) => controllerInstance.changeValue(item.name, e.target.value));
+
+      const defaultOptionEl = document.createElement("option");
+      defaultOptionEl.setAttribute("value", 0);
+      defaultOptionEl.innerHTML = "Select...";
+
+      selectEl.appendChild(defaultOptionEl);
+    
+      item.options.map(option => {
+        const optionEl = document.createElement("option");
+        optionEl.setAttribute("value", option.value);
+        optionEl.innerHTML = option.text;
+    
+        selectEl.appendChild(optionEl);
+      });
+    
+      divEl.appendChild(selectEl);
+    
+      filtersOptions.push(divEl);
+    });
+    
+    filtersOptions.map(filter => {
+      filtersForm.appendChild(filter);
+    });
+    
+
+  }
 }
 
-let filtersOptions = [];
-
-filters.map((item, index) => {
-
-  const divEl = document.createElement("div");
-
-  const imgEl = document.createElement("img");
-  imgEl.setAttribute("src", item.image);
-  imgEl.setAttribute("alt", item.name);
-
-  divEl.appendChild(imgEl);
-
-  const labelEl = document.createElement("label");
-  labelEl.innerText = `${index + 1}. ${item.label}`;
-
-  divEl.appendChild(labelEl);
-
-  const selectEl = document.createElement("select");
-  selectEl.addEventListener("change", changeValue);
-
-  item.options.map(option => {
-    const optionEl = document.createElement("option");
-    optionEl.setAttribute("value", option.value);
-    optionEl.innerHTML = option.text;
-
-    selectEl.appendChild(optionEl);
-  });
-
-  divEl.appendChild(selectEl);
-
-  filtersOptions.push(divEl);
-});
-
-filtersOptions.map(filter => {
-  filtersForm.appendChild(filter);
-});
+export default filtersView;
