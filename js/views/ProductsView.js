0c23771612652c8017f2ebfae7f409d1277394a1
polvora-fosforo <contaparacodigo999@gmail.com>
2021-02-12 17:18:28 -0300

    dinamic products tags

--- js/views/ProductsView.js
+++ js/views/ProductsView.js
@@ -1,3 +1,15 @@
+// sun img
+import high from '../../images/icons/high-sun.svg';
+import low from '../../images/icons/low-sun.svg';
+import no from '../../images/icons/no-sun.svg';
+// water img
+import rarely from '../../images/icons/1-drop.svg';
+import regularly from '../../images/icons/2-drops.svg';
+import daily from '../../images/icons/3-drops.svg';
+// pet img
+import pet from '../../images/icons/pet.svg';
+import toxicity from '../../images/icons/toxic.svg';
+
 class ProductsView {
   constructor(controllerInstance) {
     this.controllerInstance = controllerInstance;
@@ -20,6 +32,8 @@ class ProductsView {
     document.querySelector("#noResults").setAttribute("class", "hidden");
     document.querySelector("#results").setAttribute("class", "results");
     const resultsPicks = document.querySelector("#resultsPicks");
+
+    resultsPicks.innerHTML = '';
 
     this.products.map(product => {
       // PRODUCT DIV
@@ -52,6 +66,34 @@ class ProductsView {
       productPriceEl.innerText = "$" + product.price;
 
       productInfoEl.appendChild(productPriceEl);
+
+      // PRODUCT TAGS
+      const productTagsEl = document.createElement("div");
+      
+      const sunTagEl = document.createElement("img");
+      sunTagEl.setAttribute("class", "product__tag");
+      if(product.sun === "high") sunTagEl.setAttribute("src", high);
+      if(product.sun === "low") sunTagEl.setAttribute("src", low);
+      if(product.sun === "no") sunTagEl.setAttribute("src", no);
+      productTagsEl.appendChild(sunTagEl);
+
+      const waterTagEl = document.createElement("img");
+      waterTagEl.setAttribute("class", "product__tag");
+      if(product.water === "daily") waterTagEl.setAttribute("src", daily);
+      if(product.water === "regularly") waterTagEl.setAttribute("src", regularly);
+      if(product.water === "rarely") waterTagEl.setAttribute("src", rarely);
+      productTagsEl.appendChild(waterTagEl);
+
+      const petTagEl = document.createElement("img");
+      petTagEl.setAttribute("class", "product__tag");
+      if(product.toxicity) {
+        petTagEl.setAttribute("src", toxicity);
+      } else {
+        petTagEl.setAttribute("src", pet);
+      };
+      productTagsEl.appendChild(petTagEl);
+
+      productInfoEl.appendChild(productTagsEl);
 
       // INSERTING THE PRODUCT AT THE PICKS
       resultsPicks.appendChild(productEl);
