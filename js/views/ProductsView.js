52ea0b49ac1e2d67b272d88bea5695c55110cf31
polvora-fosforo <contaparacodigo999@gmail.com>
2021-02-13 10:23:23 -0300

    making grid layout for destop products dilpays

--- js/views/ProductsView.js
+++ js/views/ProductsView.js
@@ -37,10 +37,16 @@ class ProductsView {
 
     this.products = [{"id":1,"name":"Euphorbia eritrea","sun":"high","water":"rarely","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/euphorbia-eritrea.png","price":25,"toxicity":false,"staff_favorite":true},{"id":2,"name":"Succulent Bowl","sun":"high","water":"rarely","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/succulent-bowl.png","price":30,"toxicity":false,"staff_favorite":false},{"id":3,"name":"Bunny ears cacti","sun":"high","water":"rarely","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/bunny-ears-cacti.png","price":20,"toxicity":false,"staff_favorite":false},{"id":4,"name":"Ficus lyrata","sun":"high","water":"regularly","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/ficus-lyrata.png","price":30,"toxicity":false,"staff_favorite":false},{"id":8,"name":"Calathea triostar","sun":"no","water":"daily","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/calathea-triostar.png","price":50,"toxicity":true,"staff_favorite":false},{"id":9,"name":"Monstera deliciosa","sun":"no","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/monstera-deliciosa.png","water":"daily","price":50,"toxicity":true,"staff_favorite":true},{"id":5,"name":"Bamboo","sun":"low","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/lucky-bamboo.png","water":"regularly","price":15,"toxicity":false,"staff_favorite":false},{"id":6,"name":"Ponytail Palm","sun":"low","water":"regularly","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/ponytail-palm.png","price":50,"toxicity":false,"staff_favorite":false},{"id":8,"name":"Calathea triostar","sun":"no","water":"daily","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/calathea-triostar.png","price":50,"toxicity":true,"staff_favorite":false},{"id":9,"name":"Monstera deliciosa","sun":"no","url":"https://storage.googleapis.com/front-br-challenges.appspot.com/green-thumb-v2/plants/monstera-deliciosa.png","water":"daily","price":50,"toxicity":true,"staff_favorite":true}]
 
-    this.products.map(product => {
+    this.products.map((product, index) => {
       // PRODUCT DIV
       const productEl = document.createElement("div");
-      productEl.setAttribute("class", "product");
+      let productClass = "";
+      if(index === 0) {
+        productClass = "product highlight"
+      } else {
+        productClass = "product"
+      }
+      productEl.setAttribute("class", productClass);
   
       // PRODUCT IMAGE
       const productImageEl = document.createElement("img");
